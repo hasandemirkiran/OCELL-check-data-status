baf99e91d027636a29894b4f6d8a0f6a19ad98de
hasan-OCELL <hasan.demirkiran@ocell.io>
2021-11-30 17:25:54 +0100

    check json file

---
+++ check_json.py
@@ -0,0 +1,22 @@
+import json 
+
+def check_general_areas(file):
+    return
+
+def tree_type_infos(file):
+    return
+
+def plot_areas(file):
+    return
+
+def roads(file):
+    return
+
+def offline_region(file):
+    return
+
+def check_files():
+    return
+
+if __name__ == '__main__':
+    check_files()
